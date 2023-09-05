8253a24c0e54a606e28af49aecfb7cae50e9244c
Prem <premdasvm8@gmail.com>
2023-09-05 15:10:41 +0530

    (chore): Project configuration updated.

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,24 +1,24 @@
 module.exports = {
-	arrowParens: "avoid",
-	bracketSameLine: true,
-	bracketSpacing: true,
-	singleQuote: false,
-	trailingComma: "all",
-	useTabs: true,
-	tabWidth: 4,
-	endOfLine: "lf",
-	htmlWhitespaceSensitivity: "css",
-	printWidth: 100,
-	proseWrap: "preserve",
-	requirePragma: false,
-	semi: true,
-	vueIndentScriptAndStyle: true,
-	overrides: [
-		{
-			files: "*.json",
-			options: {
-				printWidth: 200,
-			},
-		},
-	],
+  arrowParens: "avoid",
+  bracketSameLine: true,
+  bracketSpacing: true,
+  singleQuote: false,
+  trailingComma: "all",
+  useTabs: true,
+  tabWidth: 4,
+  endOfLine: "lf",
+  htmlWhitespaceSensitivity: "css",
+  printWidth: 100,
+  proseWrap: "preserve",
+  requirePragma: false,
+  semi: true,
+  vueIndentScriptAndStyle: true,
+  overrides: [
+    {
+      files: "*.json",
+      options: {
+        printWidth: 200,
+      },
+    },
+  ],
 };
